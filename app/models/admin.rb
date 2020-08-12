c3ecf7a9e2014a83fac5e722fa9c52745a96b78d
morgancollado <Morgan.collado@gmail.com>
2020-08-12 18:35:16 -0500

    adds helper methods

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,5 @@
 class Admin < ActiveRecord::Base
+    validates_uniqueness_of :username, case_senstive: false
+    validates_uniqueness_of :email, case_senstive: false
     has_secure_password
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+    validates_uniqueness_of :username, case_senstive: false
+    validates_uniqueness_of :email, case_senstive: false
     has_secure_password
     has_many :tickets
 end
