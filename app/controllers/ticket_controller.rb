cbcdacf48311c739c7aa6ed64e37042ca4b0f143
morgancollado <Morgan.collado@gmail.com>
2020-08-21 22:32:12 -0500

    adds styling to index, admin edit, show and admin views

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -2,7 +2,7 @@ class TicketController < ApplicationController
 
     get '/tickets' do
         if logged_in? && current_user.admin?
-            @tickets = Ticket.all
+            @tickets = Ticket.where(resolved?: false)
             erb :'/tickets/index'
         elsif logged_in?
             @tickets = Ticket.where(customer_id: session[:user_id])
